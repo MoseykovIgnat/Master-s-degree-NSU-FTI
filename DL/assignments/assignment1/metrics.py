ad086e90f151e76ae6df75a59030f3e1471bb623
Ignat Moseykov <Moseyjr99@gmail.com>
2022-03-04 23:42:16 +0700

    DL Task 1

---
+++ DL/assignments/assignment1/metrics.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+def binary_classification_metrics(prediction, ground_truth):
+    '''
+    Computes metrics for binary classification
+
+    Arguments:
+    prediction, np array of bool (num_samples) - model predictions
+    ground_truth, np array of bool (num_samples) - true labels
+
+    Returns:
+    precision, recall, f1, accuracy - classification metrics
+    '''
+    precision = 0
+    recall = 0
+    accuracy = 0
+    f1 = 0
+    
+    fn = np.sum(prediction[ground_truth == True] == False)
+    tp = np.sum(prediction[ground_truth == True] == True)
+    tn = np.sum(prediction[ground_truth == False] == False)
+    fp = np.sum(prediction[ground_truth == False] == True)
+    
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    accuracy = (tp + tn) / (tp + tn + fp + fn)
+    f1 = 2 * precision * recall / (precision + recall) 
+
+    return precision, recall, f1, accuracy
+
+
+def multiclass_accuracy(prediction, ground_truth):
+    '''
+    Computes metrics for multiclass classification
+
+    Arguments:
+    prediction, np array of int (num_samples) - model predictions
+    ground_truth, np array of int (num_samples) - true labels
+
+    Returns:
+    accuracy - ratio of accurate predictions to total samples
+    '''
+    # TODO: Implement computing accuracy
+    accuracy = sum((prediction == ground_truth)) / len(prediction)
+    return accuracy
